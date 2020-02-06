cda33e800affaa144d78cfd88d1a76bad1384a3b
brentahrens1 <cidida@gmail.com>
2020-02-06 11:03:58 -0800

    removes the phone number from contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,6 @@ const Contact = props => (
         <h1 className="contact-title">Contact</h1>
         <h3 className="contact-location">Los Angeles, CA</h3>
         <h3 className="contact-email"><a href="mailto:brentholmesahrens@gmail.com">brentholmesahrens@gmail.com</a></h3>
-        <h3 className="contact-phone"><a href="tel:301-606-3561">301.606.3561</a></h3>
         <h3 className="contact-social"><a href="https://www.linkedin.com/in/brenthahrens/" target="_blank">Linkedin</a></h3>
         <p><a href="#home">Back to the top</a></p>
     </div>
