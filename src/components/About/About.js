a2459267c96e3dcd390ac774b366eeff097f3d10
Brent Ahrens <cidida@gmail.com>
2020-03-03 13:16:17 -0800

    updates skills

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = props => (
                 <img className="about-profile" src={Profile} alt="profile"></img>
                 <h1 className="about-name"><a href="mailto:brentholmesahrens@gmail.com" target="_blank" rel="noopener noreferrer">Brent Ahrens</a></h1>
                 <h2 className="about-title">Front End Web Developer</h2>
-                <h4 className="about-skills">React | Vue | Javascript | CSS3 | SASS | CSS Grid | FlexBox | HTML5 | RESTful APIs | BEM | Github | SEO | Wordpress | Squarespace | Wagtail | Google Analytics | Gatsby</h4>
+                <h4 className="about-skills">React | Vue | Javascript | CSS3 | SASS | CSS Grid | FlexBox | HTML5 | RESTful APIs | BEM | Github | SEO | Wordpress | Squarespace | Wagtail | Google Analytics | Gatsby | Command Line</h4>
                 <p className="about-description">I’m a front-end developer eager to work in a demanding and dynamic role that compliments my passion for building responsive and functional websites and leverages my extensive experience working in teams.</p>
             </div>
         </div>
