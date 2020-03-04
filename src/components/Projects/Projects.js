9d74ce43fd80f0c199bffa5ca4e40dec3c45d8c3
Brent Ahrens <cidida@gmail.com>
2020-03-03 20:08:29 -0800

    updates mdll description

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -33,7 +33,7 @@ const Projects = props => (
             <a href="https://maisondelaluz.com/" target="_blank" rel="noopener noreferrer"><img src={MDLL} alt="maison de la luz" className="project__item--img"></img></a>
             <h3 className="project__item--title"><a href="https://maisondelaluz.com/" target="_blank" rel="noopener noreferrer">Maison De La Luz</a></h3>
             <p className="project__item--skills"><i>Javascript / CSS / HTML / Wordpress</i></p>
-            <p className="project__item--description">A luxury hotel in New Orleans. In collaboration with our design team, I built the site using custom code blocks within the Wordpress framework.</p>
+            <p className="project__item--description">A luxury hotel in New Orleans. I work on Javascript and CSS enhancements within the Wordpress framework.</p>
         </div>
         <div className="project__item project__item--5">
             <a href="https://www.lastlight.nyc/" target="_blank" rel="noopener noreferrer"><img src={LastLight} alt="last light" className="project__item--img"></img></a>
@@ -45,7 +45,7 @@ const Projects = props => (
             <a href="https://www.barmarilou.com/" target="_blank" rel="noopener noreferrer"><img src={BarMarilou} alt="bar marilou" className="project__item--img"></img></a>
             <h3 className="project__item--title"><a href="https://www.barmarilou.com/" target="_blank" rel="noopener noreferrer">Bar Marilou</a></h3>
             <p className="project__item--skills"><i>Javascript / CSS / HTML / Squarespace</i></p>
-            <p className="project__item--description">A spirited cocktail bar in the heart of New Orleans. In collaboration with our design team, I built the site using custom code blocks within the Squarespace framework.</p>
+            <p className="project__item--description">A spirited cocktail bar in the heart of New Orleans. In collaboration with our design team, I  the site using custom code blocks within the Squarespace framework.</p>
         </div>
         <div className="project__item project__item--7">
             <a href="https://www.diabeticteststrips4cash.com/" target="_blank" rel="noopener noreferrer"><img src={DiabeticStrips} alt="db medical supplies" className="project__item--img"></img></a>
